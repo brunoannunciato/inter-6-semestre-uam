f664827b6d87e87147a509b7f3adc712c4e70359
Bruno Annunciato <brunobannunciato@gmail.com>
2018-11-04 22:35:20 -0200

    create header + initial configurations

---
+++ src/header.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <?php  include('includes.php')?>
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <h1 class="title">LOGO</h1>
+                <div class="add-profile">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25">
+                        <defs>
+                            <style>
+                                .cls-1,.cls-2,.cls-4{fill:none}.cls-1,.cls-2{stroke:#fff;stroke-width:2px}.cls-2{stroke-linecap:round}.cls-3{stroke:none}
+                            </style>
+                        </defs>
+                        <g id="Group_6" data-name="Group 6" transform="translate(-333 -7)">
+                            <g id="Rectangle_1" class="cls-1" data-name="Rectangle 1" transform="translate(333 7)">
+                                <rect width="25" height="25" class="cls-3" rx="5"/>
+                                <rect width="23" height="23" x="1" y="1" class="cls-4" rx="4"/>
+                            </g>
+                            <g id="Group_5" data-name="Group 5" transform="translate(5.783 -53.217)">
+                                <path id="Line_1" d="M0 0v14.434" class="cls-2" data-name="Line 1" transform="translate(339.717 65.5)"/>
+                                <path id="Line_2" d="M0 0v14.434" class="cls-2" data-name="Line 2" transform="rotate(90 137.109 209.826)"/>
+                            </g>
+                        </g>
+                    </svg>
+                </div>
+            </div>
+        </header>
---
+++ src/includes.php
@@ -0,0 +1,2 @@
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,400,700i,800" rel="stylesheet">
+<link rel="stylesheet" href="css/style.css">
