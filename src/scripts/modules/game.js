4f13e57f1376da9871731d729339389c2a616653
Gabriel Henrique <gabhenrique0@gmail.com>
2018-11-26 23:24:33 -0200

    Add counter to watch the sorted letter positions and compare with the total clicked items to compare and redirect the user to the next step

--- src/scripts/modules/game.js
+++ src/scripts/modules/game.js
@@ -10,7 +10,17 @@ export class Game {
 
 	leds() {
 		const leds = [$('#1'), $('#2'), $('#3'), $('#4'), $('#5'), $('#6'), $('#7'), $('#8'),$('#9'), $('#10'), $('#11'), $('#12'), $('#13'), $('#14'), $('#15'), $('#16'), $('#17'), $('#18'), $('#19'), $('#20'), $('#21'), $('#22'), $('#23'), $('#24'), $('#25'), $('#26'), $('#27'), $('#28'), $('#29'), $('#30'), $('#31'), $('#32'), $('#33'), $('#34'), $('#35'), $('#36'), $('#37'), $('#38'), $('#39'), $('#40'), $('#41'), $('#42'), $('#43'), $('#44'), $('#45'), $('#46'), $('#47'), $('#48'), $('#49'), $('#50'), $('#51'), $('#52'), $('#53'), $('#54'), $('#55'), $('#56'), $('#57'), $('#58'), $('#59'), $('#60'), $('#61'), $('#62'), $('#63'), $('#64') ]
-		const scope = this;
+		const infoIndex = Math.floor(Math.random() * Math.floor(3));
+		const sortedObj = infos[infoIndex];
+		let sortedTotal = 0, clickedTotal = 0;
+		
+		for (let value in sortedObj.values) {
+			if (sortedObj.values[value] > 0) {
+				sortedTotal++;
+			}
+		}
+		
+		console.log('sortedTotal', sortedTotal);
 
 		for (let led of leds) {
 			led.text(led.attr('id'))
@@ -49,7 +59,14 @@ export class Game {
 				let itemColor = Number(self.attr('data-color'));
 				let itemId = Number(self.attr('id'));
 
-				itemColor === infos[0].values[itemId] && infos[0].values[itemId] !== 0 ? led.addClass('checked') : console.log(`${itemColor} is not equal ${infos[0].values[itemId]}`);
+				if (itemColor === sortedObj.values[itemId] && sortedObj.values[itemId] !== 0) {
+					led.addClass('checked');
+					clickedTotal++;
+
+					clickedTotal === sortedTotal ? $('.button.done').addClass('finish') : console.log(`${clickedTotal} is smaller than ${sortedTotal}`);
+				} else {
+					console.log(`${itemColor} is not equal ${sortedObj.values[itemId]}`);
+				}
 			});
 		}
 	}
