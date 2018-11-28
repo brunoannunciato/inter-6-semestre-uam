e66f2529626325bead96fef5d9d03c0a81e0e9b4
Gabriel Henrique <gabhenrique0@gmail.com>
2018-11-27 23:57:45 -0200

    Create function to list all registered profiles when the page load and a function to insert the new profile when we click on button

--- src/index.php
+++ src/index.php
@@ -1,78 +1,66 @@
 <?php include('header.php'); ?>
 
 <main class="home">
-    <div class="container">
-        <section class="no-profiles">
-            <img src="images/home-estrela.png" alt="Icone de estrela triste" class="img">
-        
-            <p class="text">
-                Ops! parece que você não possui nenhum perfil cadastrado!
-            </p>
-    
-            <div class="button create-profile-cta">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22">
-                <defs>
-                    <style>
-                        .cls-1{fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:2px}
-                    </style>
-                </defs>
-                <g id="plus-circle" transform="translate(-1 -1)">
-                    <circle id="Ellipse_3" cx="10" cy="10" r="10" class="cls-1" data-name="Ellipse 3" transform="translate(2 2)"/>
-                    <path id="Line_3" d="M0 0v8" class="cls-1" data-name="Line 3" transform="translate(12 8)"/>
-                    <path id="Line_4" d="M0 0h8" class="cls-1" data-name="Line 4" transform="translate(8 12)"/>
-                </g>
-            </svg>
-                Adicionar Perfil
-            </div>
-        </section>
+	<div class="container">
+		<section class="no-profiles">
+			<img src="images/home-estrela.png" alt="Icone de estrela triste" class="img">
+		
+			<p class="text">
+				Ops! parece que você não possui nenhum perfil cadastrado!
+			</p>
+	
+			<div class="button create-profile-cta">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22">
+				<defs>
+					<style>
+						.cls-1{fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:2px}
+					</style>
+				</defs>
+				<g id="plus-circle" transform="translate(-1 -1)">
+					<circle id="Ellipse_3" cx="10" cy="10" r="10" class="cls-1" data-name="Ellipse 3" transform="translate(2 2)"/>
+					<path id="Line_3" d="M0 0v8" class="cls-1" data-name="Line 3" transform="translate(12 8)"/>
+					<path id="Line_4" d="M0 0h8" class="cls-1" data-name="Line 4" transform="translate(8 12)"/>
+				</g>
+			</svg>
+				Adicionar Perfil
+			</div>
+		</section>
 
-        <section class="profiles-shelf hide">
-            <div class="container">
-                <div>
-                    <p class="text">
-                        Selecione seu perfil:
-                    </p>
-                    <ul class="profiles">
-                        <li class="profile">
-                            <input type="radio" name="profile" id="1" value="João Victor Sena">
-                            <label for="1">
-                                <span class="profile-id">1</span>
-                                <p class="text name">
-                                    João Victor Sena
-                                </p>
-                            </label>
-                        </li>
-                    </ul>
-                </div>
-                <div class="button play">
-                    Jogar
-                </div>
-            </div>
-        </section>
+		<section class="profiles-shelf hide">
+			<div class="container">
+				<div>
+					<p class="text">Selecione seu perfil:</p>
 
-        <section class="create-profile hide">
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="back-button" viewBox="0 0 16 16">
-                    <defs>
-                        <style>
-                            .cls-1{fill:none;stroke:#84a2e8;stroke-linecap:round;stroke-linejoin:round;stroke-width:2px}
-                        </style>
-                    </defs>
-                    <g id="arrow-left" transform="translate(-4 -4)">
-                        <path id="Line_6" d="M14 0H0" class="cls-1" data-name="Line 6" transform="translate(5 12)"/>
-                        <path id="Path_5" d="M12 19l-7-7 7-7" class="cls-1" data-name="Path 5"/>
-                    </g>
-                </svg>
-                <h2 class="title">
-                    Qual o seu nome?
-                </h2>
-                <input type="text" class="name" id="input-name">
-            </div>
+					<ul class="profiles"></ul>
+				</div>
 
-            <div class="button create">Criar perfil</div>
-        </section>
+				<div class="button play">Jogar</div>
+			</div>
+		</section>
 
-    </div>
+		<section class="create-profile hide">
+			<div>
+				<svg xmlns="http://www.w3.org/2000/svg" class="back-button" viewBox="0 0 16 16">
+					<defs>
+						<style>
+							.cls-1{fill:none;stroke:#84a2e8;stroke-linecap:round;stroke-linejoin:round;stroke-width:2px}
+						</style>
+					</defs>
+					<g id="arrow-left" transform="translate(-4 -4)">
+						<path id="Line_6" d="M14 0H0" class="cls-1" data-name="Line 6" transform="translate(5 12)"/>
+						<path id="Path_5" d="M12 19l-7-7 7-7" class="cls-1" data-name="Path 5"/>
+					</g>
+				</svg>
+				<h2 class="title">
+					Qual o seu nome?
+				</h2>
+				<input type="text" class="name" id="input-name">
+			</div>
+
+			<div class="button create">Criar perfil</div>
+		</section>
+
+	</div>
 </main>
 
 <?php include('footer.php'); ?>
--- src/scripts/modules/profiles.js
+++ src/scripts/modules/profiles.js
@@ -1,20 +1,20 @@
 export class Profiles {
 	setup() {
-		const
-			noProfiles = $('.no-profiles'),
-			profilesShelf = $('.profiles-shelf'),
-			createProfile = $('.create-profile');
+		const noProfiles = $('.no-profiles');
+		const profilesShelf = $('.profiles-shelf');
+		const createProfile = $('.create-profile');
+		const profilesList = $('.profiles');
+		const buttonPlay = $('.button.play');
 
-		// const changePage = (function() {
-
-		// 	if (!localStorage.profiles) {
-		// 		noProfiles.removeClass('hide');
-		// 		profilesShelf.addClass('hide');
-		// 	} else {
-		// 		profilesShelf.RemoveClass('hide');
-		// 		noProfiles.addClass('hide');
-		// 	}
-		// })()
+		const changePage = (function() {
+			if (!localStorage.getItem('profiles')) {
+				noProfiles.removeClass('hide');
+				profilesShelf.addClass('hide');
+			} else {
+				profilesShelf.removeClass('hide');
+				noProfiles.addClass('hide');
+			}
+		})();
 
 		const ableCreateProfile = (function() {
 			$('.add-profile, .create-profile-cta').click(() => {
@@ -22,45 +22,64 @@ export class Profiles {
 				profilesShelf.addClass('hide');
 				createProfile.removeClass('hide');
 			});
-		})()
+		})();
+
+		const appendProfileItem = function(listNumber, name) {
+			profilesList.append(`
+				<li class="profile">
+					<input type="radio" name="profile" id="${listNumber}" value="${name}">
+
+					<label for="${listNumber}">
+						<span class="profile-id">${listNumber}</span>
+						<p class="text name">${name}</p>
+					</label>
+				</li>
+			`);
+		};
 
 		const loadProfiles = (function() {
-			// let storageProfiles = localStorage.profiles;
-			// let profilesJson = JSON.parse(storageProfiles);
-			// console.log(profilesJson);
-		})()
+			let storageProfiles = JSON.parse(localStorage.getItem('profiles'));
+
+			if (storageProfiles) {
+				$(storageProfiles.names).each((index, element) => appendProfileItem(index + 1, element));
+				buttonPlay.addClass('active');
+			}
+		})();
+
+		const listLastProfile = function() {
+			let storageProfiles = JSON.parse(localStorage.getItem('profiles'));
+			let lastItemCount = storageProfiles.names.length;
+
+			appendProfileItem(lastItemCount, storageProfiles.names[lastItemCount - 1]);
+			buttonPlay.addClass('active');
+		};
 
 		const addProfile = (function() {
 			const
 				inputName = $('#input-name'),
 				button = $('.button.create');
 
-			inputName.keydown(() => {
-				if (inputName.val().length > 0) {
-					button.addClass('active');
-				} else {
-					button.removeClass('active');
-				}
-			});
+			inputName.keydown(() => inputName.val().length > 0 ? button.addClass('active') : button.removeClass('active'));
 
 			button.click(() => {
 				let profilesStorage = localStorage.getItem('profiles');
 				let values = profilesStorage ? profilesStorage.substr(profilesStorage.indexOf('[') + 1) : '';
 				let valuesNew = values ? values.substr(0, values.indexOf(']')) : '';
 
-				profilesStorage ? localStorage.setItem('profiles', `{'names': [${valuesNew}, '${inputName.val()}']}`) : localStorage.setItem('profiles', `{'names': ['${inputName.val()}']}`);
+				profilesStorage ? localStorage.setItem("profiles", `{"names": [${valuesNew}, "${inputName.val()}"]}`) : localStorage.setItem("profiles", `{"names": ["${inputName.val()}"]}`);
 				
 				createProfile.addClass('hide');
 				profilesShelf.removeClass('hide');
-			});
 
-		})()
+				listLastProfile();
+			});
+		})();
 
 		const startGame = (function() {
 			$('.button.play').click(() => {
 				sessionStorage.setItem('logged', $('[name=profile]'));
 				window.location.replace('./panel.php');
 			})
-		})()
+		})();
 	}
 }
