21acbcde32129929bb9d27e5e2de8450763df918
Gabriel Henrique <gabhenrique0@gmail.com>
2018-11-27 22:35:49 -0200

    Change the sort number to the array infos length

--- src/scripts/modules/game.js
+++ src/scripts/modules/game.js
@@ -10,7 +10,7 @@ export class Game {
 
 	leds() {
 		const leds = [$('#1'), $('#2'), $('#3'), $('#4'), $('#5'), $('#6'), $('#7'), $('#8'),$('#9'), $('#10'), $('#11'), $('#12'), $('#13'), $('#14'), $('#15'), $('#16'), $('#17'), $('#18'), $('#19'), $('#20'), $('#21'), $('#22'), $('#23'), $('#24'), $('#25'), $('#26'), $('#27'), $('#28'), $('#29'), $('#30'), $('#31'), $('#32'), $('#33'), $('#34'), $('#35'), $('#36'), $('#37'), $('#38'), $('#39'), $('#40'), $('#41'), $('#42'), $('#43'), $('#44'), $('#45'), $('#46'), $('#47'), $('#48'), $('#49'), $('#50'), $('#51'), $('#52'), $('#53'), $('#54'), $('#55'), $('#56'), $('#57'), $('#58'), $('#59'), $('#60'), $('#61'), $('#62'), $('#63'), $('#64') ]
-		const infoIndex = Math.floor(Math.random() * Math.floor(3));
+		const infoIndex = Math.floor(Math.random() * Math.floor(infos.length));
 		const sortedObj = infos[infoIndex];
 		let sortedTotal = 0, clickedTotal = 0;
 
@@ -82,9 +82,8 @@ export class Game {
 
 					if (clickedTotal === sortedTotal) {
 						$('.button.done').removeClass('disable').addClass('active');
-						$(leds).each((i, obj) => {
-							obj.addClass('disable');
-						})
+
+						$(leds).each((i, obj) => obj.addClass('disable'));
 					} else {
 						console.log(`${clickedTotal} is smaller than ${sortedTotal}`);
 					}
